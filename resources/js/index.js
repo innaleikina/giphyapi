e108381976bdc4652f1ca975a01fbb2af84fb597
Inna <designbyinna@gmail.com>
2018-06-23 20:21:39 -0600

    working on mobile functionality

--- resources/js/index.js
+++ resources/js/index.js
@@ -13,17 +13,49 @@ $("#add-search-term").click(function () {
         var newButton = $("<button>");
         var searchTerm = $("#search-term").val().trim();
         topics.push(searchTerm);
-        generateButtons();
+        if ($(window).width() > 768) {
+            generateButtons();
+        } else{
+
+            generateButtonsMobile();
+        }
+        
     } else {
         alert("Please enter a search term");
     }
 
 })
 
+
 function generateButtons() {
     $("#btn-container").empty();
+    // Looping through the array of movies
     for (var i = 0; i < topics.length; i++) {
-        $("#btn-container").append('<button class="btn-style btn" value="' + topics[i] + '">' + topics[i] + '</button>');
+        var btn = $("<button>");
+        // Adding a class of movie-btn to our button
+        btn.addClass("btn-style");
+        btn.addClass("btn");
+        btn.attr("value", topics[i]);
+        // Providing the initial button text
+        btn.text(topics[i]);
+        // Adding the button to the buttons-view div
+        $("#btn-container").append(btn);
+    }
+}
+
+function generateButtonsMobile() {
+    $("#btn-container").empty();
+    if (btnsMobileVisible == false) {
+        for (var i = 0; i < topics.length; i++) {
+            $(".mobile-btns").append('<button class="btn-style btn-mobile" value="' + topics[i] + '" >' + topics[i] + '</button>');
+            $(".btn-mobile").css('display', "block");
+            btnsMobileVisible = true;
+            $(".btn-mobile").css("background-color", "rgb(39, 62, 71)");
+        }
+    } else if (btnsMobileVisible == true) {
+        // $(".mobile-btns").css("background-color", "none");
+        $(".btn-mobile").css('display', "none");
+        btnsMobileVisible = false;
     }
 }
 
@@ -45,7 +77,7 @@ function getGifs() {
             container.append(img);
             var p = $("<div>");
             p.addClass("rating");
-            p.text(result.data[i].rating.toUpperCase())
+            p.text("rating is : " + result.data[i].rating.toUpperCase())
             container.append(p);
             $(".grid-container").append(container);
         }
@@ -64,5 +96,13 @@ function switchGifs() {
     }
 }
 
+var btnsMobileVisible = false
+
+$("#mobile-btn-menu").click(function () {
+    console.log("mobile menu pressed");
+    generateButtonsMobile();
+})
+
 $(document).on("click", ".btn", getGifs);
+$(document).on("click", ".btn-mobile", getGifs);
 $(document).on("click", ".one-gif", switchGifs);
