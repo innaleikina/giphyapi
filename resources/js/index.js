f940950caf67dc7087923afaf6588068088648c9
Inna <designbyinna@gmail.com>
2018-06-24 19:33:23 -0600

    mobile buttons complete

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,6 +1,6 @@
 var key = "jYTfzlUXqx607nu91cb320GgiFkNNk6T";
 
-var topics = ["soccer", "football", "tennis", "badminton", "gymnastics", "baseball", "track", "swimming", "cheerleading", "dancing", "curling"];
+var topics = ["soccer", "football", "tennis", "badminton", "gymnastics", "baseball", "track", "swimming", "dancing", "curling"];
 
 var state;
 var searchTerm;
@@ -9,6 +9,8 @@ var searchPressed = false;
 //display buttons on screen for desktop version
 generateButtons();
 
+
+// _________________________ FUNCTION PERFORED WHEN SEARCH IS PRESSED_____________________________
 //when search term is added a new button is appended, and buttons rerender on screen
 //gifs populate when search is pressed
 $("#add-search-term").click(function () {
@@ -20,6 +22,8 @@ $("#add-search-term").click(function () {
         var newButton = $("<button>");
         searchTerm = $("#search-term").val().trim();
         topics.push(searchTerm);
+
+        //generate buttons based on screen size
         if ($(window).width() > 768) {
             generateButtons();
             getGifs();
@@ -32,9 +36,7 @@ $("#add-search-term").click(function () {
 
 })
 
-
-
-
+//__________________________________________GENERATE BUTTONS UNIVERSAL___________________________
 //generated desktop buttons
 function generateButtons() {
     $("#btn-container").empty();
@@ -48,6 +50,7 @@ function generateButtons() {
         btn.attr("value", topics[i]);
         // Providing the initial button text
         btn.text(topics[i]);
+
         // Adding the button to the buttons-view div
         $("#btn-container").append(btn);
     }
@@ -55,37 +58,48 @@ function generateButtons() {
 
 
 
-
-var btnsMobileVisible = false
+//__________________________________________MOBILE BUTTONS___________________________
+var btnsMobileVisible = false;
 
 //generates mobile buttons and appends them to mobile container
 //toggles the visibility of mobile buttons
 function generateButtonsMobile() {
+    console.log(btnsMobileVisible);
     $(".display-mobile-btns").empty();
-    $(".btn-mobile").css("display", "none");
+    // $(".display-mobile-btns").css("display", "none");
     if (btnsMobileVisible == false) {
         for (var i = 0; i < topics.length; i++) {
-            $(".display-mobile-btns").append('<button class="btn-style btn-mobile" value="' + topics[i] + '" >' + topics[i] + '</button>');
-            $(".btn-mobile").css('display', "block");
-            btnsMobileVisible = true;
+            $(".display-mobile-btns").append('<button class="btn-style btn-mobile" value="' + topics[i] + '" >' + topics[i] + ' </button>');
+            $(".display-mobile-btns").css('display', "block");
             $(".btn-mobile").css("background-color", "rgb(39, 62, 71)");
+            btnsMobileVisible = true;
         }
     } else if (btnsMobileVisible == true) {
         // $(".mobile-btns").css("background-color", "none");
-        $(".btn-mobile").css('display', "none");
+        $(".display-mobile-btns").css('display', "none");
         btnsMobileVisible = false;
     }
 }
 
 
-
+//__________________________________________MOBILE MENU BUTTON CLICK GENERATES BUTTONS__________________________
 //toggles mobile buttons on click of the mobile-btn-menu button
 $("#mobile-btn-menu").click(function () {
-    console.log("mobile menu pressed");
+   // console.log("mobile menu pressed");
     generateButtonsMobile();
 })
 
 
+//__________________________________________HIDE MOBILE BUTTONS __________________________________
+function hideMobileBtns() {
+    // console.log("button shod")
+    $(".display-mobile-btns").css('display', "none");
+    btnsMobileVisible = false;
+}
+
+
+
+//__________________________________________RETRIEVE GIFS MOBILE AND DESKTOP___________________________
 //get Gifs on button press
 function getGifs() {
     $(".grid-container").empty();
@@ -124,6 +138,7 @@ function getGifs() {
 }
 
 
+//__________________________________________TOGGLE BETWEEN STILL AND MOVING GIFS___________________________
 //toggles moving and still gifs
 function switchGifs() {
     state = $(this).attr("data-state");
@@ -138,15 +153,11 @@ function switchGifs() {
 
 
 
-
+//__________________________________________CALL FUNCTIONS BUTTONS___________________________
 //function calls1
 $(document).on("click", ".btn", getGifs);
 $(document).on("click", ".btn-mobile", getGifs);
-$(document).on("click", ".btn-mobile", hideMobileBtn);
+$(document).on("click", ".btn-mobile", hideMobileBtns);
 $(document).on("click", ".one-gif", switchGifs);
 
 
-function hideMobileBtn() {
-    $(".btn-mobile").css('display', "none");
-    btnsMobileVisible = false;
-}
